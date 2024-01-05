a4c7c39cf5e5d3120ee801b9570dfb02a9c4b1cd
ig <ig@igs-MacBook-Pro.lan>
2024-01-04 19:15:24 -0800

    Handle form submission and simple input sanitization

--- register.php
+++ register.php
@@ -1,3 +1,47 @@
+<?php
+
+function sanitizeFormPassword($inputText)
+{
+    $inputText = strip_tags($inputText);
+    return $inputText;
+}
+
+function sanitizeFormUsername($inputText)
+{
+    $inputText = strip_tags($inputText);
+    $inputText = str_replace(' ', '', $inputText);
+    return $inputText;
+}
+
+function sanitizeFormString($inputText)
+{
+    $inputText = strip_tags($inputText);
+    $inputText = str_replace(' ', '', $inputText);
+    $inputText = ucfirst(strtolower($inputText));
+    return $inputText;
+}
+if (isset($_POST['loginButton'])) {
+    //Login button was pressed
+    echo "login button was pressed";
+}
+if (isset($_POST['registerButton'])) {
+    //Register button was pressed
+    $username = sanitizeFormUsername($_POST['username']);
+
+    $firstName = sanitizeFormString($_POST['firstName']);
+
+    $lastName = sanitizeFormString($_POST['lastName']);
+
+    $email = sanitizeFormString($_POST['email']);
+
+    $email2 = sanitizeFormString($_POST['email2']);
+
+    $password = sanitizeFormPassword($_POST['password']);
+
+
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
